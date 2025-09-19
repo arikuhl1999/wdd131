a84720cae9c76ed498681d2e4e3fb35a445983bc
arikuhl1999 <arielakuhl1999@byui.edu>
2025-09-19 17:13:08 -0600

    added html and css for mission statement

---
+++ ponder/meta-css.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+let radius = 3;
+// instead of using var use let
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
